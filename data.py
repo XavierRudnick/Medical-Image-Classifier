b6210e15061130b43c0f56d0e18d9cb4bf7c71b6
Xavier Rudnick <83371628+XavierRudnick@users.noreply.github.com>
2024-11-07 14:08:02 -0700

    Update data.py

--- data.py
+++ data.py
@@ -6,7 +6,8 @@ from sklearn.model_selection import train_test_split
 from PIL import Image
 import matplotlib.pyplot as plt
 import time
-#putting all image paths and labels in seperate numpy arrays
+
+#loading all image paths and labels in seperate arrays
 folder_path = "chest_xray/train/NORMAL"
 jpgs = [os.path.join(root, file)
         for root, dirs, files in os.walk(folder_path)
@@ -25,22 +26,40 @@ label2 = [1] * m
 label.extend(label2)
 jpgs.extend(jpgs2) 
 
-#processing the images into arrays
-array_list = [np.squeeze(tf.keras.preprocessing.image.img_to_array(tf.keras.preprocessing.image.load_img(path, color_mode='grayscale',target_size=(150,150))), axis=-1) for path in jpgs]
-arr = np.array(array_list)
-label_np = np.array(label)
+#splitting dataframes into train and test groups
+train_x, test_x, train_y, test_y = train_test_split(jpgs, label, test_size=0.1)
 
-#splitting processed arrays into train and test groups
-train_x, test_x, train_y, test_y = train_test_split(arr, label_np, test_size=0.1)
+#convert dataframes to tensor flow obj
+train_df = tf.data.Dataset.from_tensor_slices((train_x, train_y))
+test_df = tf.data.Dataset.from_tensor_slices((test_x, test_y))
 
-
+#classes of the dataframe
 class_names = [
     "NORMAL",
     "PNEUMONIA",
 ]
 
-train_x=train_x/255.0
-test_x=test_x/255.0
+#prepare dataset by converting to tensor obj, resizing, and normalizing
+def prepare_dataset(path, label):
+    image = tf.io.read_file(path) #load image
+    image = tf.image.decode_jpeg(image, channels=1) #convert jpeg to tensor obj
+    image = tf.image.resize(image, [150, 150]) #resize
+    image = image / 255.0 #convert values to [0,1] range
+    return image, label
+
+#use tensorflow dynamic tuning to optimize the performance of pipeline
+AUTOTUNE = tf.data.AUTOTUNE
+
+#apply prepare_dataset function to each tensorflow obj, use num_parallel_calls to optimize preformence
+train_ds = train_df.map(prepare_dataset, num_parallel_calls=AUTOTUNE)
+test_ds = test_df.map(prepare_dataset, num_parallel_calls=AUTOTUNE)
+
+BATCH_SIZE = 32
+
+#shuffle to avoid unwanted trends in image order, group images in batches for better speeds, prefetch new batch
+train_ds = train_ds.shuffle(1000).batch(BATCH_SIZE).prefetch(AUTOTUNE)
+test_ds = test_ds.batch(BATCH_SIZE).prefetch(AUTOTUNE)
+
 #creating model
 model=tf.keras.Sequential([
     tf.keras.layers.Conv2D(64,(3,3),activation='relu',input_shape=(150,150,1)),
@@ -54,14 +73,15 @@ model=tf.keras.Sequential([
     tf.keras.layers.Dropout(0.5),
     tf.keras.layers.Dense(10,activation='softmax'),
 ])
+
 #compiling with SparseCategoricalCrossentropy loss function
 model.compile(optimizer='adam',
              loss=tf.keras.losses.SparseCategoricalCrossentropy(from_logits=True),
              metrics=['accuracy'])
 
 model.summary()
-model.fit(train_x, train_y, epochs=1)
+model.fit(train_ds, epochs=5)
 
-test_loss, test_acc = model.evaluate(test_x, test_y, verbose=1)
+test_loss, test_acc = model.evaluate(test_ds, verbose=1)
 print('\nTest accuracy:', test_acc)
 print('\nTest loss:', test_loss)
